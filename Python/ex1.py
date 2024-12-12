259616509519504346cf8cb08c8df50c5d8d9a66
Subash Kumar <subashsk11831@gmail.com>
2024-12-12 20:50:09 +0530

    2024-12-12T15:20:09.692Z:

--- Python/ex1.py
+++ Python/ex1.py
@@ -1,8 +1,20 @@
+print("")
 x = 5
 print('The number is ' + str(x))
 
 print("")
 print ("=======================================================")
-print
+print("")
+
 sum = 1 + 5
 print(sum)
+
+print("")
+print ("=======================================================")
+print("")
+
+print("show this in the console")
+
+print("")
+print ("=======================================================")
+print("")
