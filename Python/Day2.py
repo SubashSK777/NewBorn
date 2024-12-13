e63363ac6303e069c91afb4b638eb902fd464080
Subash Kumar <subashsk11831@gmail.com>
2024-12-13 07:57:36 +0530

    2024-12-13T02:27:36.095Z:

--- Python/Day2.py
+++ Python/Day2.py
@@ -1,109 +1,111 @@
-from datetime import date
+# from datetime import date
 
-print("Today's Date is: " + str(date.today()))
+# print("Today's Date is: " + str(date.today()))
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-# print("Welcome to the greeter program")
-# name = input("Enter your name: ")
-# print("Greetings " + name)
+# # print("Welcome to the greeter program")
+# # name = input("Enter your name: ")
+# # print("Greetings " + name)
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-a = 27
-b = 93
-if a < b:
-    print("a is less than b")
-elif a > b:
-    print("a is greater than b")
-else: 
-    print ("a is equal to b")
+# a = 27
+# b = 93
+# if a < b:
+#     print("a is less than b")
+# elif a > b:
+#     print("a is greater than b")
+# else: 
+#     print ("a is equal to b")
     
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-a = 27
-b = 93
-c = 380
+# a = 27
+# b = 93
+# c = 380
 
-if a > b:
-  print("a is Greatest")
+# if a > b:
+#   print("a is Greatest")
   
-  if a > c:
-    print("a is the Greatest")
+#   if a > c:
+#     print("a is the Greatest")
     
-  else:
-    print("c is the Greatest")
+#   else:
+#     print("c is the Greatest")
 
-elif b > c:
-  print("b is the Greatest")
+# elif b > c:
+#   print("b is the Greatest")
   
-else:
-  print("c is the Greatest")
+# else:
+#   print("c is the Greatest")
   
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-fact = "The Moon has no atmosphere. "
-fact += "No sound can be heard on the Moon."
-print(fact)
+# fact = "The Moon has no atmosphere. "
+# fact += "No sound can be heard on the Moon."
+# print(fact)
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-print("temperatures and facts about the moon".title())  
+# print("temperatures and facts about the moon".title())  
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-temperatures = "Alakazambarkadabradubaididubidiadumwiaht"
-temperatures_list = temperatures.split("a")
-print(temperatures_list)
+# temperatures = "Alakazambarkadabradubaididubidiadumwiaht"
+# temperatures_list = temperatures.split("a")
+# print(temperatures_list)
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-print("Moon" in "This text will describe facts about the Moon")
+# print("Moon" in "This text will describe facts about the Moon")
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-text = "This text will describe facts aboutMoonthe Moon"
-count = text.count("Moon")
-print(count)
+# text = "This text will describe facts aboutMoonthe Moon"
+# count = text.count("Moon")
+# print(count)
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-temperatures = "Alakazambarkadabradubaididubidiadumwiaht"
-temperatures_list = temperatures.split("a")
-print(temperatures)
-print(temperatures_list[2:2:])
+# temperatures = "Alakazambarkadabradubaididubidiadumwiaht"
+# temperatures_list = temperatures.split("a")
+# print(temperatures)
+# print(temperatures_list[2:2:])
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
-name = ["SubashKumarK", "Kennedy", "John", "Marcus", "Killer"]
+# name = ["SubashKumarK", "Kennedy", "John", "Marcus", "Killer"]
 
-for person in name:
-  if "k" in person.lower():  
-      print(person)
+# for person in name:
+#   if "k" in person.lower():  
+#       print(person)
 
-print("")
-print ("=======================================================")
-print("")
+# print("")
+# print ("=======================================================")
+# print("")
 
 mars_temperature = "The highest temperature on Mars is about 30 C"
 
+for temps in mars_temperature:
+  print (temps)
