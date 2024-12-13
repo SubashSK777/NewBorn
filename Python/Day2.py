f1e5397b06713d70cc3a1507416b1e4a4778f813
Subash Kumar <subashsk11831@gmail.com>
2024-12-13 06:11:40 +0530

    2024-12-13T00:41:40.418Z:

--- Python/Day2.py
+++ Python/Day2.py
@@ -1,3 +1,11 @@
 from datetime import date
 
 print("Today's Date is: " + str(date.today()))
+
+print("")
+print ("=======================================================")
+print("")
+
+print("Welcome to the greeter program")
+name = input("Enter your name: ")
+print("Greetings " + name)
