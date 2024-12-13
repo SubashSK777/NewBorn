49d43a294d547703b3852d74569d0b5a4d24ce5c
Subash Kumar <subashsk11831@gmail.com>
2024-12-13 09:17:03 +0530

    2024-12-13T03:47:03.765Z:

--- Python/Day2.py
+++ Python/Day2.py
@@ -113,8 +113,8 @@
 text1 = str(input("Enter a String: "))
 text2 = str(input("Enter another String: "))
 
-char1 = list(text1).islower()
-char2 = list(text2).islower()
+char1 = (list(text1.islower())
+char2 = list(text2.islower())
 
 if sorted(char1) == sorted(char2):
   print ("It is an Anagram")
