bb5d268c11366d1ab5009173220e562e10d0b3ee
Subash Kumar <subashsk11831@gmail.com>
2024-12-13 06:27:18 +0530

    2024-12-13T00:57:18.251Z:

--- Python/Day2.py
+++ Python/Day2.py
@@ -22,3 +22,14 @@ elif a > b:
     print("a is greater than b")
 else: 
     print ("a is equal to b")
+    
+print("")
+print ("=======================================================")
+print("")
+
+a = 27
+b = 93
+c = 38
+
+if a > b:
+  print("a is Greatest")
