98647284dddc81333e9abb27d45c1c20cd52064b
Subash Kumar <subashsk11831@gmail.com>
2024-12-13 06:26:14 +0530

    2024-12-13T00:56:14.340Z:

--- Python/Day2.py
+++ Python/Day2.py
@@ -9,3 +9,16 @@ print("")
 print("Welcome to the greeter program")
 name = input("Enter your name: ")
 print("Greetings " + name)
+
+print("")
+print ("=======================================================")
+print("")
+
+a = 27
+b = 93
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else: 
+    print ("a is equal to b")
