804729dbf6db1a55d1f4dc851848c18be1877e0a
Subash Kumar <subashsk11831@gmail.com>
2024-12-14 19:21:19 +0530

    2024-12-14T13:51:19.413Z:

--- Java/AvgOfFive.java
+++ Java/AvgOfFive.java
@@ -1,6 +1,8 @@
 public class AvgOfFive {
   public static void main(String[] args) {
-    
+    System.out.println(BigOrSmall(0, 0, 0, 0, 0));
   }
-  public static 
+  public static String BigOrSmall (int a, int b, int c, int d, int e) {
+
+  }
 }
