4de79ac3049a76b5047ab30bb3143d196b541b61
Subash Kumar <subashsk11831@gmail.com>
2024-12-14 21:02:50 +0530

    2024-12-14T15:32:50.123Z:

--- Java/EvenPrinter.java
+++ Java/EvenPrinter.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class EvenPrinter {
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Number: ");
+    int n = sc.nextInt();
   }
   public static int Eventer (int n) {
     int i = 0;
