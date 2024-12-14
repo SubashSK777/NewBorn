7ca55eb233c267fed5b22825ab2ee60cbfadcd6e
Subash Kumar <subashsk11831@gmail.com>
2024-12-14 19:17:06 +0530

    2024-12-14T13:47:06.177Z:

--- Java/LargestOfThree.java
+++ Java/LargestOfThree.java
@@ -1,10 +1,16 @@
 public class LargestOfThree {
   public static void main(String[] args) {
-    System.out.println(Greatest(0, 0, 0));
+    System.out.println(Greatest(27, 56, 0));
   }
 
   public static int Greatest (int x, int y, int z) {
-    if (x > y && x > z) {}
+    if (x > y && x > z) {
+      return x;
+    } else if ( y > x && y > z) {
+      return y;
+    } else {
+      return z;
+    }
   }
   
   
