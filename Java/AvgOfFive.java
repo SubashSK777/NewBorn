73c989f74a0a33b214ef498a6a72cdc2124bedbe
Subash Kumar <subashsk11831@gmail.com>
2024-12-14 19:23:28 +0530

    2024-12-14T13:53:28.145Z:

--- Java/AvgOfFive.java
+++ Java/AvgOfFive.java
@@ -4,7 +4,11 @@ public class AvgOfFive {
   }
   public static String BigOrSmall (int a, int b, int c, int d, int e) {
     int Avg = (a + b + c + d + e) / 5;
-    
+    if (Avg > 50) {
+      return "It is Greater Than 50";
+    } else {
+      return "It is Smaller Than 50"
+    }
 
   }
 }
