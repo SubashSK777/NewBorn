ba134d36d53c60b35c4e0217432a554c0a035d92
Subash Kumar <subashsk11831@gmail.com>
2024-12-16 13:16:17 +0530

    2024-12-16T07:46:17.274Z:

--- Python/Day 4.py
+++ Python/Day 4.py
@@ -1,15 +1,20 @@
-x = int(input("Enter a Number: "))
-y = int(input("Enter another Number: "))
+# x = int(input("Enter a Number: "))
+# y = int(input("Enter another Number: "))
 
-oper = int(input(""" Select and Operation to Perform
-                 1. Addition
-                 2.Subtraction
-                 3. Exit """))
+# oper = int(input("""Select an Operation to Perform:
+# 1. Addition
+# 2. Subtraction
+# 3. Exit
+# Your choice: """))
 
-if oper == 1:
-  print("The Sum of 2 Numbers is " + (x+y))
-elif oper == 2:
-  print("The Difference of 2 Numbers is " + (x-y))
-else: 
-  break
-  
+# if oper == 1:
+#     print(f"The Sum of the two numbers is {x + y}")
+# elif oper == 2:
+#     print(f"The Difference of the two numbers is {x - y}")
+# elif oper == 3:
+#     print("Exiting the program.")
+# else:
+#     print("Invalid choice. Please try again.")
+
+
+n = int(input("Enter a Number: "))
