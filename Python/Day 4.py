53c0447ed6d40da5ba2c2ab044dc64ed15b9bfe5
Subash Kumar <subashsk11831@gmail.com>
2024-12-16 13:09:54 +0530

    2024-12-16T07:39:54.534Z:

--- Python/Day 4.py
+++ Python/Day 4.py
@@ -1,4 +1,11 @@
 x = int(input("Enter a Number: "))
 y = int(input("Enter another Number: "))
 
-oper = int(input(""" ))
+oper = int(input(""" Select and Operation to Perform
+                 1. Addition
+                 2.Subtraction
+                 3. Exit """))
+
+if oper == 1:
+  print
+  
