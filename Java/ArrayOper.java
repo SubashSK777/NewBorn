3cd223c684da0fd65ee7c124cf17b689c8f2cbc3
Subash Kumar <subashsk11831@gmail.com>
2024-12-22 12:50:17 +0530

    2024-12-22T07:20:17.355Z:

---
+++ Java/ArrayOper.java
@@ -0,0 +1,33 @@
+public class ArrayOper {
+  public static void main(String[] args) {
+    
+    int nums[] = new int[8];
+    nums[0] = 8;
+
+    System.out.println(nums[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int nums[] = {4, 6, 8, 0};
+    // for (int i = 0; i < nums.length; i++)
+    //   System.out.println(nums[i]);
+  }
+}
