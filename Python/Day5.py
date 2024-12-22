6740e32f31398bf278ca54429580018a540da40a
Subash Kumar <subashsk11831@gmail.com>
2024-12-22 13:06:11 +0530

    2024-12-22T07:36:11.317Z:

--- Python/Day5.py
+++ Python/Day5.py
@@ -4,6 +4,9 @@
 
 # ================================================================================
 
-txt = "tuaacd iawoqc huirg sozdjn iu c"
-split_txt = txt.split()
-print()
+# txt = "tuaacd iawoqc huirg sozdjn iu c"
+# split_txt = txt.split()
+# print(split_txt)
+
+# ======================================================================================
+
