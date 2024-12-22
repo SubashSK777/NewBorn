4e671c4a2d274e2ce2039d302848889e02e90712
Subash Kumar <subashsk11831@gmail.com>
2024-12-22 13:04:01 +0530

    2024-12-22T07:34:01.562Z:

--- Python/Day5.py
+++ Python/Day5.py
@@ -1,3 +1,5 @@
-txt = "wert ydfgh tyui erty fghj"
-up_txt = txt.title()
-print(up_txt)
+ # txt = "wert ydfgh tyui erty fghj"
+ # up_txt = txt.title()
+ # print(up_txt)
+
+# ================================================================================
