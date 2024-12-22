3c11b650baa435a5437cd1dc96bdaffaa1df5a63
Subash Kumar <subashsk11831@gmail.com>
2024-12-22 13:29:36 +0530

    2024-12-22T07:59:36.705Z:

--- Python/Day5.py
+++ Python/Day5.py
@@ -38,5 +38,7 @@
 
 # ================================================================================
 
-txt
+txt = "lorem is not null its 7 but also 10 while all thinks its 0"
+for num in txt.split():
+  if num.is
 
