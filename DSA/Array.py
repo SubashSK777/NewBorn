c2d29af0f93671ea997639532b99bbc8ad98280f
Subash Kumar <subashsk11831@gmail.com>
2024-12-25 16:14:04 +0530

    2024-12-25T10:44:04.690Z:

--- DSA/Array.py
+++ DSA/Array.py
@@ -3,9 +3,10 @@ from array import *
 
 arr = array('i', [1, 2, 3, 4, 5])
 
-NewArr = array(arr.typecode, (a*a for a in arr))
+NewArr = array(arr.typecode, (a*a*a*a*a* for a in arr))
 
-print(a)
+for a in NewArr:
+  print(a*a*a*a*a*a*a*a*a*a*a*a)
 
 
 
