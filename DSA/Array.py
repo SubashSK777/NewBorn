ee8d000a3ec031f771940b0223fff57ed9442c5c
Subash Kumar <subashsk11831@gmail.com>
2024-12-25 16:19:18 +0530

    2024-12-25T10:49:18.037Z:

--- DSA/Array.py
+++ DSA/Array.py
@@ -3,12 +3,21 @@ from array import *
 
 arr = array('i', [1, 2, 3, 4, 5])
 
-NewArr = array(arr.typecode, (a for a in arr))
+i = 0
 
+while i < len(arr):
+  print(arr[i])
+  i += 1
+
+
+
+
+
+
+
+# NewArr = array(arr.typecode, (a for a in arr))
 # for ab in NewArr:
 #   print(ab*ab)
-
-
 
 # for vals in arr:
 #   print(vals*vals)
