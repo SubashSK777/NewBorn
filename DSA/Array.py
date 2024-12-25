3dc7448d623daa0e40c867c5e1a84b6fce46c364
Subash Kumar <subashsk11831@gmail.com>
2024-12-25 16:24:30 +0530

    2024-12-25T10:54:30.319Z:

--- DSA/Array.py
+++ DSA/Array.py
@@ -1,17 +1,34 @@
 #ARRAYYYYYYYYYY
 from array import *
 
-arr = array('i', [1, 2, 3, 4, 5])
+arr = array('i', [])
 
-NewArr = array(arr.typecode, [])
-i = 0
+x = int(input("Enter a Number: "))
 
-while i < len(arr):
-  b = arr[i]*arr[i]
-  NewArr.append(b)
-  i += 1
 
-print(NewArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# NewArr = array(arr.typecode, [])
+
+# i = 0
+
+# while i < len(arr):
+#   b = arr[i]*arr[i]
+#   NewArr.append(b)
+#   i += 1
+
+# print(NewArr)
 
 # NewArr = array(arr.typecode, (a for a in arr))
 # for ab in NewArr:
