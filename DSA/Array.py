428c50d268a0d6721e60c1b2095958cfc2df8979
Subash Kumar <subashsk11831@gmail.com>
2024-12-25 16:02:38 +0530

    2024-12-25T10:32:38.312Z:

--- DSA/Array.py
+++ DSA/Array.py
@@ -1,12 +1,13 @@
 #ARRAYYYYYYYYYY
 from array import *
 
-arr = array('i', [1, 2, 3, 4, 5])
+# arr = array('i', [1, 2, 3, 4, 5])
 
-# arr = arr[::-1]
+# # arr = arr[::-1]
 
-for vals in arr: 
-  print(vals)
+# for vals in arr: 
+#   print(vals)
 
-for i in range(len(arr)):
-  print(arr[i])
+# for i in range(len(arr)):
+#   print(arr[i])
+
