83e5001b804d916ccb3392fba3453673f44dc576
Subash Kumar <subashsk11831@gmail.com>
2024-12-25 16:15:07 +0530

    2024-12-25T10:45:07.369Z:

--- DSA/Array.py
+++ DSA/Array.py
@@ -3,10 +3,10 @@ from array import *
 
 arr = array('i', [1, 2, 3, 4, 5])
 
-NewArr = array(arr.typecode, (a*a*a*a*a* for a in arr))
+NewArr = array(arr.typecode, (a*a*a*a*a*a*a*a for a in arr))
 
 for a in NewArr:
-  print(a*a*a*a*a*a*a*a*a*a*a*a)
+  print(*a*a*a*a*a*a*a*a*a*a*a*aa*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a)
 
 
 
