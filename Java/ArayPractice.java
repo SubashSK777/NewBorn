cf4ad1d3c098ae6476b4fa594ab24036bcc501b7
Subash Kumar <subashsk11831@gmail.com>
2024-12-27 22:14:38 +0530

    2024-12-27T16:44:38.302Z:

--- Java/ArayPractice.java
+++ Java/ArayPractice.java
@@ -1,5 +1,10 @@
 public class ArayPractice {
-  public static void main {
+  public static void main (String [] args){
+
+  }
+  public class DVD {
+    public String name;
+    public int ReleaseYear;
     
   }
 }
