179909784fef9d66f829db9a724cdb30f2ec8aa7
Subash Kumar <subashsk11831@gmail.com>
2024-12-28 21:49:47 +0530

    2024-12-28T16:19:47.237Z:

--- DSA/Mathematics.py
+++ DSA/Mathematics.py
@@ -1,7 +1,10 @@
 from math import *
 
-n = 7
+n = 91
 
-for i in range(2, isqrt(n)):
+for i in range(2, isqrt(n) + 1):
   if n % i == 0:
-    
+    print ("Not a Prime")
+    break
+  else:
+    print("Prime")
