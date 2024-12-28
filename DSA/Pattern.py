0f2a8c9ba21505165cb4b080b243355cdae10b1d
Subash Kumar <subashsk11831@gmail.com>
2024-12-28 21:31:58 +0530

    2024-12-28T16:01:57.972Z:

--- DSA/Pattern.py
+++ DSA/Pattern.py
@@ -10,15 +10,15 @@ n = 5
 #   if i % 2 != 0:
 #     print (i)
 
-for i in range(n):
-  for j in range(i + 1):
-    print ("*", end = " ")
-  print("\n")
+# for i in range(n):
+#   for j in range(i + 1):
+#     print ("*", end = " ")
+#   print("\n")
   
 
 for i in range(n):
   for j in range(n - i - 1):
     print(" ", end = " ")
-  for k in range(2*i ):
+  for k in range(2*i + 1):
     print("*", end = " ")
   print("\n")
