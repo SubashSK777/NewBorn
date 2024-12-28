f126034bbb208d326aac50ac571d1c1ea86b50ba
Subash Kumar <subashsk11831@gmail.com>
2024-12-28 20:51:35 +0530

    2024-12-28T15:21:35.550Z:

--- DSA/Pattern.py
+++ DSA/Pattern.py
@@ -1,7 +1,11 @@
-n = 5
+n = 12
 
-for i in range(1, n + 1):
-  print(i)
+# for i in range(1, n + 1):
+#   print(i)
   
-for j in range(n, 0, -1):
-  print(j)
+# for j in range(n, 0, -1):
+#   print(j)
+
+for i in range(n):
+  if i % 2 == 0:
+    print (i)
