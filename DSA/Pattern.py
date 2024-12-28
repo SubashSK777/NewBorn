a416d2556b04aa73d66be82f4cad61b9ea926e2e
Subash Kumar <subashsk11831@gmail.com>
2024-12-28 22:39:41 +0530

    2024-12-28T17:09:41.345Z:

--- DSA/Pattern.py
+++ DSA/Pattern.py
@@ -51,5 +51,10 @@ n = 5
 #   print()
   
   
-for i in range(n):
-  for j in range(n - 1 - i)
+# for i in range(n):
+#   for j in range(n - 1 - i):
+#     print(" ", end = " ")
+#   for k in range(2 * i + 1):
+#     print("*", end = " ")
+#   print()
+  
