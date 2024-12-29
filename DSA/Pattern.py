ce1b8411fea4fc955434c9fcf8cb577906eabeee
Subash Kumar <subashsk11831@gmail.com>
2024-12-29 09:24:34 +0530

    2024-12-29T03:54:34.367Z:

--- DSA/Pattern.py
+++ DSA/Pattern.py
@@ -68,5 +68,5 @@ for i in range(n):
   for j in range(n - i - 1):
     print(" ", end = " ")
   for k in range(2*i + 1):
-    print ([if turek % 2 != 0], end = " ")
+    print ([k if k % 2 != 0 else " "], end = " ")
   print()
