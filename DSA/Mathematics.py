6d1fd9e7f89981535215ae58aea3e08978b5413b
Subash Kumar <subashsk11831@gmail.com>
2024-12-29 08:48:05 +0530

    2024-12-29T03:18:05.898Z:

--- DSA/Mathematics.py
+++ DSA/Mathematics.py
@@ -62,7 +62,7 @@ from math import *
   
 # print (res)
   
-n = 91
+n = 153
 org_n = n
 size = len(str(n))
 res = 0
@@ -70,3 +70,8 @@ res = 0
 while n != 0:
   sub = n % 10
   res += sub ** size
+  n = n // 10
+  
+if res == org
+  
+print(res)
