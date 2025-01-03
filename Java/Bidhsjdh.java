33bdc2b9aa326c247d59789327f898f35d292202
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 16:00:39 +0530

    2025-01-03T10:30:39.085Z:

--- Java/Bidhsjdh.java
+++ Java/Bidhsjdh.java
@@ -1,20 +1,9 @@
 public class Bidhsjdh {
   public static void main(String[] args) {
-    int n = 7;
-    switch (n) {
-      case 1:
-        System.out.println("Monday");
-        break;
-      case 2:
-        System.out.println("Tuesday");
-        break;
-      case 3:
-        System.out.println("Wednesday");
-        break;
-      case 4:
-        System.out.println("Thurday");
-      default:
-        System.out.println("Noooooooooooooooooooooooooooo");
-    }
+    
+  }
+
+  static int Factorial(int n) {
+    
   }
 }
