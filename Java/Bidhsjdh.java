95edacc67bd9e32694d844a8ef9ab5a5e3a5a6c5
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 16:07:14 +0530

    2025-01-03T10:37:14.257Z:

--- Java/Bidhsjdh.java
+++ Java/Bidhsjdh.java
@@ -1,14 +1,5 @@
 public class Bidhsjdh {
   public static void main(String[] args) {
-   System.out.println(Facto(5));
   }
-
-  static int Facto(int n) {
-    int res = 1;
-    for ( int i = 1; i <= n; i++) {
-      res *= res*i;
-      
-    }
-    return res;
-  }
+  
 }
