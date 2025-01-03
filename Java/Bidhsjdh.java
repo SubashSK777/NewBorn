8d2b3b6815b7b6db9b4c401a3735c96032e9e132
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 16:09:20 +0530

    2025-01-03T10:39:20.783Z:

--- Java/Bidhsjdh.java
+++ Java/Bidhsjdh.java
@@ -1,5 +1,9 @@
 public class Bidhsjdh {
   public static void main(String[] args) {
+    Counter(10);
   }
-  static 
+  static int Counter (int n) {
+    System.out.printlnn);
+    Counter( n - 1);
+  }
 }
