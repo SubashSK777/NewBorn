ed64a3e11728a7b338b9a0ac18482222ae5120fc
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 14:17:16 +0530

    2025-01-03T08:47:16.006Z:

--- Java/Javam.java
+++ Java/Javam.java
@@ -1,11 +1,11 @@
 public class Javam {
 	public static void main (String [] args) {
     int age = 20;
-    int salary = 20000;
-		if (age < 18) {
-			if (salary < 25000) {
+    int salary = 30000;
+		if (age > 18) {
+			if (salary > 25000) {
 				System.out.println("Eligible for Loan");
 			}	
 		} System.out.println("Not Eligible");
   }
-	}
+}
