42dcef236f9462dafd9a3193fe91bcb492af6508
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 14:13:43 +0530

    2025-01-03T08:43:42.863Z:

--- Java/Hellllllllll2.java
+++ Java/Hellllllllll2.java
@@ -1,6 +1,5 @@
 public class Hellllllllll2 {
-  static int add (int a, int b) {
-		return a + b;
+  static void add (int a, int b) {
 		System.out.println(a + b);
 	}
 	public static void main (String [] args) {
