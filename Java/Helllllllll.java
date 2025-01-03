0ada2d22640cbcdd7c0c23c65fa3476cf9a1a7da
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 13:29:28 +0530

    2025-01-03T07:59:27.989Z:

---
+++ Java/Helllllllll.java
@@ -0,0 +1,10 @@
+public class Helllllllll {
+  public static void main(String[] args) {
+    String s = "";
+    if (s.isEmpty()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
