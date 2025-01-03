8c9c8383e1f7b9f657546acc0f2b89dfebee60b7
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 15:48:02 +0530

    2025-01-03T10:18:02.790Z:

--- Java/Bidhsjdh.java
+++ Java/Bidhsjdh.java
@@ -1,8 +1,13 @@
 public class Bidhsjdh {
   public static void main(String[] args) {
-    
+    int n = 5;
     switch (n) {
-      
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
     }
   }
 }
