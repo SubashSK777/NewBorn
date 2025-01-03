96bdafa51ec7d69a2cac89f28e924f5bf57342da
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 14:37:29 +0530

    2025-01-03T09:07:29.741Z:

--- Java/Javam.java
+++ Java/Javam.java
@@ -1,6 +1,10 @@
 public class Javam {
 	public static void main (String [] args) {
 		int n = 5;
-		for ( )
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; i++) {
+				System.out.println("*");
+			}
+		}
   }
 }
