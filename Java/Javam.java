07ced1659fd1f3225a43e00846f92293ea9d18c3
Subash Kumar <subashsk11831@gmail.com>
2025-01-03 14:29:08 +0530

    2025-01-03T08:59:08.904Z:

--- Java/Javam.java
+++ Java/Javam.java
@@ -1,6 +1,9 @@
 public class Javam {
 	public static void main (String [] args) {
+		char A = 'A';
+		char B = 'B';
+		
 		int mark = 2;
-    System.out.println('A') ? mark > 85 : System.out.println("B") ? 
+    System.out.println() ? mark > 85 : System.out.println("B") ? 
   }
 }
