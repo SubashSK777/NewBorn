a7e691ba48c0623c2b0dfa673d3d1e19b9955232
Subash Kumar <subashsk11831@gmail.com>
2025-01-04 14:38:07 +0530

    2025-01-04T09:08:07.571Z:

--- Java/uhviuh.java
+++ Java/uhviuh.java
@@ -4,6 +4,8 @@ public class uhviuh {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     List<List<Integer>> matrix = new ArrayList<>();
-    matrix.add(Array.asList)
+    matrix.add(Arrays.asList(1, 2, 3));
+    matrix.add(Arrays.asList(4, 5, 6));
+    matrix.add(Arrays.asList(null))
   }
 }
