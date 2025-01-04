9a2c792e9d1f174537a3ec2a11c8614deb555357
Subash Kumar <subashsk11831@gmail.com>
2025-01-04 16:07:00 +0530

    2025-01-04T10:37:00.778Z:

--- Java/bridv.java
+++ Java/bridv.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class bridv {
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    int [] arr;
+
+    int n = sc.
   }
 }
