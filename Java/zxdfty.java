6d5f753ece1208dfdf1f7229d82b2d8d6972026a
Subash Kumar <subashsk11831@gmail.com>
2025-01-04 05:49:16 +0530

    2025-01-04T00:19:16.558Z:

--- Java/zxdfty.java
+++ Java/zxdfty.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
+
 public class zxdfty {
-  int x = 5;
+  int x = ;
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     zxdfty myObj = new zxdfty();
     System.out.println(myObj.x);
   }
