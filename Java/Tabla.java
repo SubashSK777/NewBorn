1d623237bc4672a7e53b7f5b9e4f08824ce303b5
Subash Kumar <subashsk11831@gmail.com>
2025-01-04 16:01:42 +0530

    2025-01-04T10:31:42.257Z:

--- Java/Tabla.java
+++ Java/Tabla.java
@@ -1,13 +1,8 @@
 public class Tabla {
   public static void main(String[] args) {
-    int n = 4;
-    for (int i = 1; i < n; i++) {
-      i = i^i;
-      if (i == n) {
-        System.out.println("Perfect Square");
-      }
-    
-    }
-    System.out.println("Not a Perfect Square");
+  int n = 5;
+  for (int i = 1; i < 11; i++){
+    System.out.println(n + " x " + i + " = " + n*i);
+  }
   }
 }
