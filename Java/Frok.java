623968c07125a900fdd148d66573cfccd3ef7d3b
Subash Kumar <subashsk11831@gmail.com>
2025-01-04 14:20:25 +0530

    2025-01-04T08:50:25.292Z:

--- Java/Frok.java
+++ Java/Frok.java
@@ -5,9 +5,9 @@ public class Frok {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int sum = 0;
-    
     for (int i = 0; i < n; i++) {
-
+      
+      sum = sum + 2*i+1;
     }
   }
 }
