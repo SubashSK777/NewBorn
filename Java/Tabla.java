552896784b06c8c513d5b5b74f3f803f0b88f682
Subash Kumar <subashsk11831@gmail.com>
2025-01-04 15:07:41 +0530

    2025-01-04T09:37:41.166Z:

--- Java/Tabla.java
+++ Java/Tabla.java
@@ -1,4 +1,7 @@
-public class Tabla {public static void main(String[] args) {int n = 5;for (int i = 1; i < 11; i++){
+public class Tabla {
+public static void main(String[] args) {
+    int n = 5;
+    for (int i = 1; i < 11; i++){
       System.out.println(n + " x " + i + " = " + n*i);
     }
   }
