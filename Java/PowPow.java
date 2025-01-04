0628c2d07281f65dee6cc99965d8d387536e9ece
Subash Kumar <subashsk11831@gmail.com>
2025-01-04 15:01:18 +0530

    2025-01-04T09:31:18.543Z:

--- Java/PowPow.java
+++ Java/PowPow.java
@@ -5,14 +5,13 @@ public class PowPow {
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
-    int i = 0;
     while (n % 2 == 0){
         n = n / 2;  
     }
     if (n == 1) {
-      System.out.println("Not a Power");
-    } else {
       System.out.println("Is a Power");
+    } else {
+      System.out.println("Not a Power");
     }
   }
 }
