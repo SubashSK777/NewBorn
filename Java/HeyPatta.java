455dd05975d089ee446844f777fe1bbc37c098de
Subash Kumar <subashsk11831@gmail.com>
2025-01-05 10:58:16 +0530

    2025-01-05T05:28:16.639Z:

--- Java/HeyPatta.java
+++ Java/HeyPatta.java
@@ -10,8 +10,8 @@ public class HeyPatta {
 
   public static void Triangle(int n) {
     for (int i = 1; i < n; i++){
-      for (int j = 1; j < i; j++){
-
+      for (int j = i; j < n; j++){
+        System.out.println();
       }
     }
   }
