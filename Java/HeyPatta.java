865dffa85b6cd7fe501fed4977738a0d8d2b025d
Subash Kumar <subashsk11831@gmail.com>
2025-01-05 13:38:10 +0530

    2025-01-05T08:08:10.865Z:

--- Java/HeyPatta.java
+++ Java/HeyPatta.java
@@ -55,10 +55,13 @@ public class HeyPatta {
   }
 
   public static void Crossbreed (int n) {
-    for (int i = 0; i <= n; i ++) {
-      for (int j = 0; j <= i; j++) {
+    for (int i = 1; i <= n; i ++) {
+      for (int j = 1; j <= i; j++) {
         System.out.print(" * ");
       } System.out.println();
+    }
+    for (int i = 1; i <= n; i++) {
+      for (int j = n; i <= 1)
     }
   }
 
