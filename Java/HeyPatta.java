c9a98fc16b40d895eccf10e7ae9aa83132ba711e
Subash Kumar <subashsk11831@gmail.com>
2025-01-05 10:24:18 +0530

    2025-01-05T04:54:18.206Z:

--- Java/HeyPatta.java
+++ Java/HeyPatta.java
@@ -1,8 +1,13 @@
 public class HeyPatta {
   public static void Square(int n) {
+    for (int i = n; i < n; i++) {
+      for (int j = n; j < n; j++) {
+        System.out.print("*");
+      }
+    }
   }
 
   public static void main(String[] args) {
-    Square(0);
+    Square(5);
   }
 }
