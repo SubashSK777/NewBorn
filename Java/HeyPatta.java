ba0348885ee92abbc576673b39267cbeb084deb3
Subash Kumar <subashsk11831@gmail.com>
2025-01-05 14:10:40 +0530

    2025-01-05T08:40:40.897Z:

--- Java/HeyPatta.java
+++ Java/HeyPatta.java
@@ -66,7 +66,7 @@ public class HeyPatta {
     }
     
     for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= i ; j++) {
+      for (int j = 1; n < ; n--) {
         if (j == 1 || j == n) {
           System.out.print(" * ");
         } else {
