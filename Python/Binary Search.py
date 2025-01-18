ba90866835a040e6a68e47a96ecc3676519a7c1d
Subash Kumar <subashsk11831@gmail.com>
2025-01-18 07:59:09 +0530

    2025-01-18T02:29:09.551Z:

--- Python/Binary Search.py
+++ Python/Binary Search.py
@@ -1,6 +1,13 @@
 arr = [-5, -3, -2, 1, 3, 5]
 
 def BS(arr, target):
-  N = 
+  N = len(arr)
   L = 0
-  R = 
+  R = N
+  
+  while L < R:
+    M = (L +R) // 2
+    if arr[M] == target:
+      return M
+    elif arr[M] < target:
+      
