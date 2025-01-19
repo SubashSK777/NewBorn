1edf2be48bc9681b7af79dc0a53e6cdc3b36514b
Subash Kumar <subashsk11831@gmail.com>
2025-01-19 09:16:27 +0530

    2025-01-19T03:46:27.737Z:

--- Python/Day 4.py
+++ Python/Day 4.py
@@ -1,15 +1,8 @@
-candies = [2,3,5,1,3] 
-n_candies = candies.copy()
-extraCandies = 3
+flowerbed = [1,0,0,0,1] 
+n = 1
 
-res = []
-n = len(candies)
+F = S - 1
+S = None
+T = S + 1
 
-for i in range(n):
-    n_candies[i] += extraCandies
-    if n_candies[i] >= max(candies):
-        res.append(True)
-    else:
-        res.append(False)
-
-print(res)
+for i range
