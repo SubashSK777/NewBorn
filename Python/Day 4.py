66a71fe83d295d69a30d0083889e2d482c56fca8
Subash Kumar <subashsk11831@gmail.com>
2025-01-20 13:57:02 +0530

    2025-01-20T08:27:02.688Z:

--- Python/Day 4.py
+++ Python/Day 4.py
@@ -1,4 +1,6 @@
 nums = [20,100,10,12,5,13]
 
 for i in range(len(nums)):
-    if max(nums[:i]) < nums        
+    if min(nums[:i]) < nums[i] and max(nums[i:]) > nums[i]:
+        print("True")
+print("False")      
