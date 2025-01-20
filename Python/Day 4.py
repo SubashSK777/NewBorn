2364fdf14273f1814794a080b58b5a117e18c689
Subash Kumar <subashsk11831@gmail.com>
2025-01-20 13:55:57 +0530

    2025-01-20T08:25:57.515Z:

--- Python/Day 4.py
+++ Python/Day 4.py
@@ -1,11 +1,4 @@
 nums = [20,100,10,12,5,13]
 
 for i in range(len(nums)):
-    for j in range(i, len(nums)):
-        if nums[i] < nums[j]:
-            for k in range(j, len(nums)):
-                if nums[j] < nums[k]:
-                    print("True")
-                    
-print("Error")
-        
+    if max(nums[:i]) < nums        
