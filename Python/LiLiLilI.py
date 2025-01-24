4805eb0aa67035712ceecb0fd65dcb7eb3382e46
Subash Kumar <subashsk11831@gmail.com>
2025-01-24 19:03:01 +0530

    2025-01-24T13:33:01.836Z:

--- Python/LiLiLilI.py
+++ Python/LiLiLilI.py
@@ -1,13 +1,2 @@
 class Node:
-  def __init__(self, data, next):
-    self.data = data
-    self.next = None
-    
-    
-class LinkedList:
-  def __init__(self, head):
-    self.head = None
-    
   
-    
-    
