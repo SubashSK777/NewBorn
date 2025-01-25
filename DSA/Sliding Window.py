fa402e037637d2c601887f0e01f2a3c437185b35
Subash Kumar <subashsk11831@gmail.com>
2025-01-25 10:40:48 +0530

    2025-01-25T05:10:48.354Z:

--- DSA/Sliding Window.py
+++ DSA/Sliding Window.py
@@ -1,4 +1,5 @@
 arr = [2, 1, 5, 1, 3, 2]
+n = len(arr)
 k = 3
 
 curr_sum = 0
@@ -6,5 +7,11 @@ curr_sum = 0
 for i in range(k):
   curr_sum += arr[i]
   
-max_avg = curr_sum
+max_avg = curr_sum / n
+
+for i in range(k, n):
+  curr_sum += arr[i]
+  curr_sum -= arr[n - k]
+  
+  max_avg = 
   
