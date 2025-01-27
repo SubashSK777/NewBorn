efa13b8faf88e1116cfbeb1bd452ce8c9e2d1f53
Subash Kumar <subashsk11831@gmail.com>
2025-01-27 18:35:22 +0530

    2025-01-27T13:05:22.312Z:

--- Python/Binary Search.py
+++ Python/Binary Search.py
@@ -1,4 +1,11 @@
-arr = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+arr = []
+for i in matrix:
+  for j in matrix:
+    arr.append
+  
+
 target = 10
 
 def binary_search(arr, target):
