2b027fb7b2acea86bedec056bc21078eb1c25357
Subash Kumar <subashsk11831@gmail.com>
2025-01-27 15:24:42 +0530

    2025-01-27T09:54:41.941Z:

--- Python/Binary Search.py
+++ Python/Binary Search.py
@@ -1,20 +1,2 @@
 arr = [1, 2, 2, 2, 3, 4, 5]
 
-def binary_search(arr, target):
-	L = 0
-	R = len(arr) - 1
-	while L <= R:
-		M = (L + R)//2
-		if arr[M]  == target:
-			return M 
-		elif arr[M] < target:
-			L = M + 1
-		else:
-			R = M - 1
-
-	return -1 
-
-
-res = binary_search(arr, 2)
-
-print(res)
