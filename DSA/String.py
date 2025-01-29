1b2c0eed62869447c5d7dd99e0f328a3d9f3f3d4
Subash Kumar <subashsk11831@gmail.com>
2025-01-29 18:15:04 +0530

    2025-01-29T12:45:04.103Z:

--- DSA/String.py
+++ DSA/String.py
@@ -1,14 +1,13 @@
 arr = [1,2,3,4]
-
 p = 1
-
 s = 1
-
 res = [1]
 
-for i in range(len(arr)):
-  p *= 
-  res.append[p]
+for i in range(len(arr) - 1):
+  p *= arr[i]
+  res.append(p)
+  
+print(res)
   
   
 
