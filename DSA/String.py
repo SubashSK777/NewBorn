f40b420a70ca76f21dadbb11dca1321ddbd0d7fd
Subash Kumar <subashsk11831@gmail.com>
2025-01-30 15:28:56 +0530

    2025-01-30T09:58:56.270Z:

--- DSA/String.py
+++ DSA/String.py
@@ -2,4 +2,11 @@ arr = [1,2,4,8,8,8,8,8,6,5,3,2]
 target = 8
 #first occurance
 def bi_search(arr, target):
+  l = 0
+  r = len(arr) - 1
   
+  while l < r:
+    m  =(l + r) // 2
+    if arr[m] == target:
+      r = m
+    
