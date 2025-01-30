fdc9cf1674904cf694601825963fc8777bf0746b
Subash Kumar <subashsk11831@gmail.com>
2025-01-30 15:30:00 +0530

    2025-01-30T09:59:59.966Z:

--- DSA/String.py
+++ DSA/String.py
@@ -1,7 +1,7 @@
 arr = [1,2,4,8,8,8,8,8,6,5,3,2]
 target = 8
 #first occurance
-def bi_search(arr, target):
+def fbi_search(arr, target):
   l = 0
   r = len(arr) - 1
   
@@ -9,4 +9,14 @@ def bi_search(arr, target):
     m  =(l + r) // 2
     if arr[m] == target:
       r = m
-    
+    else:
+      l = m + 1
+  return l
+
+
+def lbi_search(arr, target):
+  l = 0 
+  r = len(nums)
+
+
+print(fbi_search(arr, target))
