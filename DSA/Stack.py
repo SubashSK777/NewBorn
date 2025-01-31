4dd6b31526966bc99e44cb6bc51fff4a4bd5e304
Subash Kumar <subashsk11831@gmail.com>
2025-01-31 10:07:37 +0530

    2025-01-31T04:37:37.671Z:

--- DSA/Stack.py
+++ DSA/Stack.py
@@ -2,3 +2,10 @@ class Stack:
   def __init__(self):
     self.stack = []
     
+  def append(self, element):
+    self.stack.append(element)
+    
+  def pop(self, element):
+    self.stack.append(element)
+    
+  def peek(self)
