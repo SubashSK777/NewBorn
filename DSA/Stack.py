5b9e2cdaf576fff0ead6d551e82155e3c1ff03dc
Subash Kumar <subashsk11831@gmail.com>
2025-01-31 13:24:06 +0530

    2025-01-31T07:54:06.681Z:

--- DSA/Stack.py
+++ DSA/Stack.py
@@ -23,10 +23,50 @@ class StackArray:
     else:
       return len(self)
     
-list1 = StackArray()
+# list1 = StackArray()
 
-list1.add(3)
-list1.add(5)
-list1.peek()
+# list1.add(3)
+# list1.add(5)
+# print(list1.peek())
 
-print(list1.stack)
+# print(list1.stack)
+
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+    
+class LinkedLi:
+  def __init__(self):
+    self.head = None
+    
+  def push(self, data):
+    new_node = Node(data)
+    new_node.next = self.head
+    self.head = new_node
+    
+  def pop(self, data):
+    popped = self.head
+    self.head = self.head.next
+    return popped
+    
+  def printer(self):
+    temp = self.head
+    while temp:
+      print(temp.data, end=" -> ")
+      temp = temp.next
+    print ("None")
+    
+    
+lili = LinkedLi()
+
+lili.push(5)
+lili.push(3)
+lili.push(9)
+lili.push(10)
+
+lili.printer()
+
+    
+
+    
