e4d71270f801e5a5f12f28242fa4e4aa071178be
Subash Kumar <subashsk11831@gmail.com>
2025-01-31 10:39:07 +0530

    2025-01-31T05:09:07.780Z:

--- DSA/Stack.py
+++ DSA/Stack.py
@@ -1,36 +1,3 @@
 class StackArray:
   def __init__(self):
-    self.stack = []
-    
-  def add(self, element):
-    self.stack.append(element)
-    
-  def pop(self, element):
-    if self.isEmpty():
-      return "Stack is Empty"
-    self.stack.pop(element)
-    
-  def peek(self):
-    if self.isEmpty():
-      return "Stack is Empty"
-    else:
-      return self.stack[-1]
-    
-  def length(self):
-    return len(self.stack)
-  
-  def Empty(self):
-    return len(self.stack) == 0
-  
-  
-list1 = StackArray()
-
-list1.add(7)
-list1.add(4)
-list1.add(8)
-print(list1.stack)
-  
-  
-
-  
-  
+    pass
