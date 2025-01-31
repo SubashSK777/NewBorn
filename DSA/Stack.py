d5a7c122e4f797ed5c72114429407f8a4a66c732
Subash Kumar <subashsk11831@gmail.com>
2025-01-31 10:06:32 +0530

    2025-01-31T04:36:32.791Z:

--- DSA/Stack.py
+++ DSA/Stack.py
@@ -1,21 +1,4 @@
-stack = []
-
-stack.append(1)
-stack.append(2)
-stack.append(3)
-
-print("Stack:", stack)
-
-element = stack.pop()
-
-print("Pop:", element)
-
-peek = stack[-1]
-
-print("Peek:", peek)
-
-print("Stack:", stack)
-
-isEmpty = not bool(stack)
-
-print(isEmpty)
+class Stack:
+  def __init__(self):
+    self.stack = []
+    
