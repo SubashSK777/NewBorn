22daefef47d9a1e0ba96d68c309fb5685d4098c4
SubashSK777 <subashsk11831@gmail.com>
2025-02-24 05:05:27 +0530

    feat: update 1 files (Python/Patt.py) at 23:35

--- Python/Patt.py
+++ Python/Patt.py
@@ -37,8 +37,9 @@ def rev_num_tri(n):
 def mid_tri(n):
     for i in range(n, -1, -1):
         for j in range(i):
-            print(" ",)
-
+            print(" ", end=" ")
+            
+        print()
 
 #square(5)
 #triangle(5)
