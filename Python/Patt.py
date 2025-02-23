3719349559751fe3ab7cc18d8ec90fc12607fe91
SubashSK777 <subashsk11831@gmail.com>
2025-02-24 04:56:01 +0530

    added Patt.py to Python

---
+++ Python/Patt.py
@@ -0,0 +1,45 @@
+def square(n):
+    for i in range(n):
+        for j in range(n):
+            print("*", end=" ")
+        print()
+
+def triangle(n):
+    for i in range(n):
+        for j in range(i + 1):
+            print("*", end=" ")
+        print()
+
+def num_triangle(n):
+    for i in range(n):
+        for j in range(i + 1):
+            print(j + 1, end=" ")
+        print()
+
+def same_num_triangle(n):
+    for i in range(n):
+        for j in range(i + 1):
+            print(i + 1, end=" ")
+        print()
+
+def rev_triangle(n):
+    for i in range(n, -1, -1):
+        for j in range(i):
+            print("*", end= " ")
+        print()
+
+def rev_num_tri(n):
+    for i in range(n, -1, -1):
+        for j in range(i):
+            print(j + 1, end=" ")
+        print()
+
+def mid_tri(n):
+    
+
+#square(5)
+#triangle(5)
+#num_triangle(5)
+#same_num_triangle(5)
+#rev_triangle(5)
+rev_num_tri(5)
